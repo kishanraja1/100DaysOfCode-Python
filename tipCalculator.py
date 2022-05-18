6a3c099f7038a99f9c1b4505afe2a7b66f76477f
Kishan Raja <kishan.raja96@gmail.com>
2022-05-18 19:02:58 -0400

    tip calc finished

--- tipCalculator.py
+++ tipCalculator.py
@@ -1,15 +1,15 @@
 #if bill is 150, split by 5 people with 12% tip
 #each person should pay (150/5)*1.12
-
+print("Welcome to the tip calculator!")
 bill = float(input("What was the total bill? "))
 people = float(input("How many people are splitting the bill? "))
-tip = float(input("How much tip would you like to leave? "))
+tip = float(input("What percentage tip would you like to leave? "))
 
 # print(type(bill))
 # print(people)
 # print(tip)
 
-total = str((bill / people) * (1 +(tip/100)))
+total = str(round((bill / people) * (1 +(tip/100)),2))
 
 
 
