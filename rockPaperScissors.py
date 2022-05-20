2de18bc480029a2439bce93426d9ba888275b543
Kishan Raja <kishan.raja96@gmail.com>
2022-05-19 22:40:00 -0400

    player can make a choice

---
+++ rockPaperScissors.py
@@ -0,0 +1,9 @@
+options = ['rock', 'paper', 'scissors']
+
+#Player making a choice
+player_choice = input('What do you choose? 0 for Rock, 1 for paper, 2 for scissors: ')
+
+player_choice = options[int(player_choice)]
+
+
+#Computer making a choice
