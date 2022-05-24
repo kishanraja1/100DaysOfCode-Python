746c529de41262429953ce36ef6ef9c85f8275e5
Kishan Raja <kraja@experientgroup.com>
2022-05-24 18:18:06 -0400

    starter code

---
+++ OOP_Coffee_Machine/main.py
@@ -0,0 +1,27 @@
+# from turtle import Turtle, Screen
+#
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("blue", "cyan")
+# timmy.forward(100)
+#
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+# from prettytable import PrettyTable
+#
+#
+# table = PrettyTable()
+# table.field_names = ["Pokemon", "Type"]
+# table.add_row(["Mewtew", "Magic"])
+# table.add_row(["Pikachu", "Electric"])
+# table.add_row(["Charmander", "water"])
+# table.align = 'l'
+# print(table)
+
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
