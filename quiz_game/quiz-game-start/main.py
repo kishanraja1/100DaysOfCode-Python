8a618c7feb474ede0bb911d52e1b5d21d80c47e2
Kishan Raja <kraja@experientgroup.com>
2022-05-28 12:18:31 -0400

    looping and methods are working

--- quiz_game/quiz-game-start/main.py
+++ quiz_game/quiz-game-start/main.py
@@ -1,12 +1,14 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 
 for question in question_data:
-    # print(question['text'])
-    # print(question['answer'])
     question_bank.append(Question(question['text'], question['answer']))
 
+quiz = QuizBrain(question_bank)
 
-print(question_bank[0].question)
+while quiz.still_has_questions():
+    quiz.next_question()
+    quiz.question_number += 1
